a7a6b4f2c5915e6880196b66ffb1ed5a17cc1819
jtseng20 <jtseng20@icloud.com>
2020-11-20 00:15:56 -0500

    Update the line-matching algorithm

--- detect.py
+++ detect.py
@@ -2,8 +2,11 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
+DEBUG = False
 DIST_THRESHOLD = 10
 TEMPLATE_SCALE = 1.5
+AREA_THRESHOLD = 0.2
+table_template_points = (TEMPLATE_SCALE*np.array([[0,0],[515,0],[515,295],[0,295]])).reshape(-1,1,2)
 
 def createLineIterator(P1, P2, img):
     imageH = img.shape[0]
@@ -70,21 +73,44 @@ def createLineIterator(P1, P2, img):
 def dist(x1,y1,x2,y2):
     return ((x2-x1)**2 + (y2-y1)**2)**0.5
 
+def isBetween(ix,iy,x1,y1,x2,y2):
+    return ((x1 <= ix <= x2) or (x1 >= ix >= x2)) and ((y1 <= iy <= y2) or (y1 >= iy >= y2))
+    
+def isClose(slope1, slope2):
+    if abs(slope1) > 10: # for steep slopes 
+        if abs(1/slope1 - 1/slope2) < 0.1:
+            return True
+    elif abs(slope1) > 1: # for medium slopes
+        if abs(slope1/slope2 - 1) < 0.2:
+            return True
+    else: # for shallow slopes
+        if abs(slope1 - slope2) < 0.1:
+            return True
+    return False
+
 def isInLine(L1,L2):
     x1,y1,x2,y2 = L1
     x3,y3,x4,y4 = L2
     
+    slope1 = (y2-y1 + 1e-8)/(x2-x1+1e-8)
+    slope2 = (y4-y3 + 1e-8)/(x4-x3+1e-8)
+    
     mx1, my1 = (x1+x2)/2, (y1+y2)/2
     mx2, my2 = (x3+x4)/2, (y3+y4)/2
     
-    slope1 = (y2-y1)/(x2-x1+1e-8)
-    slope2 = (my2-my1)/(mx2-mx1+1e-8)
+    # Slopes gotta be similar
+    if not isClose(slope1, slope2):
+        return False
     
-    if abs(slope2/slope1 - 1) < 0.2 or abs(slope2 - slope1) < 0.05:
+    # return early if midpoints are close
+    if dist(mx1,my1,mx2,my2) < DIST_THRESHOLD:
         return True
     
-    slope3 = (y4-y3)/(x4-x3+1e-8)
-    return dist(mx2,my2,mx1,my1) < DIST_THRESHOLD and abs(slope3/slope1 - 1) < 0.2
+    # return whether the second line has a point that's close to the midpoint of the first
+    ix1, iy1 = line_intersection((mx1,my1,mx1+1,my1-(1/slope1)), L2)
+    ix2, iy2 = line_intersection((mx2,my2,mx2+1,my2-(1/slope2)), L1)
+    
+    return dist(ix1,iy1, mx1,my1) < DIST_THRESHOLD or dist(ix2,iy2, mx2,my2) < DIST_THRESHOLD
 
 def allFalse(l, ll):
     for u in ll:
@@ -96,20 +122,21 @@ def combineLines(L1, L2):
     x1,y1,x2,y2 = L1
     x3,y3,x4,y4 = L2
     slope = (y2-y1)/(x2-x1+1e-8)
+    pointsArray = np.array([[x1,y1],[x2,y2],[x3,y3],[x4,y4]])
     
     # steep line, minimize/maximize y
     if abs(slope) > 1:
-        minStart = (x1,y1) if y1 < y3 else (x3,y3)
-        maxEnd = (x2,y2) if y2 > y4 else (x4,y4)
-        return np.array([minStart[0],minStart[1], maxEnd[0],maxEnd[1]])
+        minStart = pointsArray[np.argmin(pointsArray[:,1])]
+        maxEnd = pointsArray[np.argmax(pointsArray[:,1])]
+        return np.hstack((minStart,maxEnd))
     
-    minStart = (x1,y1) if x1 < x3 else (x3,y3)
-    maxEnd = (x2,y2) if x2 > x4 else (x4,y4)
-    return np.array([minStart[0],minStart[1], maxEnd[0],maxEnd[1]])
+    minStart = pointsArray[np.argmin(pointsArray[:,0])]
+    maxEnd = pointsArray[np.argmax(pointsArray[:,0])]
+    return np.hstack((minStart,maxEnd))
 
 def getAbsSlope(L):
     x1,y1,x2,y2 = L
-    return abs((y2-y1)/(x2-x1))
+    return abs((y2-y1)/(x2-x1+1e-8))
     
 def filterLines(lines):
     uniqueLines = []
@@ -126,9 +153,9 @@ def filterLines(lines):
     # Sort by absolute slope to make sure parallel pairs are together
     uniqueLines.sort(key = getAbsSlope)
     return uniqueLines
+        
 
-def line_intersection(A: np.ndarray, B: np.ndarray) -> np.ndarray:
-
+def line_intersection(A, B):
     # Intersection point between lines
     out = np.zeros(2)
     
@@ -137,122 +164,92 @@ def line_intersection(A: np.ndarray, B: np.ndarray) -> np.ndarray:
     
     out[0] = ((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
     out[1] = ((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4))/((x1-x2)*(y3-y4)-(y1-y2)*(x3-x4))
-
-    assert out.shape == (2,)
-    assert out.dtype == np.float
-
+    
     return int(out[0]),int(out[1])
 
 def getCorners(lines):
-    return np.array([line_intersection(lines[0], lines[2]),line_intersection(lines[0], lines[3]),line_intersection(lines[1], lines[2]),line_intersection(lines[1], lines[3])])
+    l = np.array([line_intersection(lines[0], lines[2]),line_intersection(lines[0], lines[3]),line_intersection(lines[1], lines[2]),line_intersection(lines[1], lines[3])])
+    # process to order points in clockwise order >:D
+    # first, get point closest to origin
+    startingPoint = np.argmin(np.linalg.norm(l,axis=1))
+    s = l[startingPoint]
+    l = np.delete(l,startingPoint,0)
+    shifted = l - s
+    # then, sort by angle to the first point
+    sc = np.vstack((s, l[np.argsort(np.arctan2(shifted[:,1], shifted[:,0]))]))
+    
+    # and then ... maybe rotate once to make sure first edge is longest
+    if np.linalg.norm(sc[1] - sc[0]) < np.linalg.norm(sc[2] - sc[1]):
+        sc = np.roll(sc, -1, axis=0)
+    return sc
 
 def drawCorners(img, corners, color = (0,255,0), radius = 5):
     for corner in corners:
         cv.circle(img, (corner[0],corner[1]), radius, color, 5)
         
+def getTableMask(img):
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    low_green = np.array([25, 52, 72])
+    high_green = np.array([102, 255, 255])
+    mask = cv.inRange(hsv, low_green, high_green)
+    kernel = np.ones((5, 5), np.uint8)
+    mask = cv.erode(mask, kernel, iterations=1)
+    return mask
+    
+def findCornersAndTransform(img, template):
+    outputRect = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
+    totalArea = img.shape[0]*img.shape[1]
+    
+    mask = getTableMask(img)
+    contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
+    for cnt in contours:
+        area = cv.contourArea(cnt)
+        approx = cv.convexHull(cnt)
+
+        if area > totalArea*AREA_THRESHOLD:
+            cv.drawContours(outputRect, [approx], 0, 255, 2)
+            #cv.fillPoly(outputRect, [approx], 255)
+
+    lines = cv.HoughLinesP(outputRect,1,np.pi/180,100,minLineLength=100,maxLineGap=10)
+    filteredLines = filterLines(lines)
+    
+    if len(filteredLines) != 4 or DEBUG:
+        print("Filtered {} lines down to {}".format(len(lines), len(filteredLines)))
+        out = np.zeros(img.shape, dtype=np.uint8)
+        print(filteredLines)
+        for line in filteredLines:
+            x1,y1,x2,y2 = line
+            cv.line(out,(x1,y1),(x2,y2),(0,255,0),1)
+            
+        print("Found {} edges".format(len(filteredLines)))
+        return out
+    
+    corners = np.float32(getCorners(filteredLines)).reshape(-1,1,2)
+    M, _ = cv.findHomography(corners, template, 0)
+    
+    return corners, M
+    
 #############################################################
 
-img4 = cv.imread('testFrames/table9.jpg')
-outputRect = np.zeros((img4.shape[0], img4.shape[1]), dtype=np.uint8)
-hsv = cv.cvtColor(img4, cv.COLOR_BGR2HSV)
+for i in range(8,14):
+    img = cv.imread('testFrames/table'+str(i)+'.jpg')
+    worldSpace = np.zeros((int(295*TEMPLATE_SCALE),int(515*TEMPLATE_SCALE),3), dtype=np.uint8)
+    output = findCornersAndTransform(img, table_template_points)
 
-low_green = np.array([25, 52, 72])
-high_green = np.array([102, 255, 255])
-green_mask = cv.inRange(hsv, low_green, high_green)
-green = cv.bitwise_and(img4, img4, mask=green_mask)
+    if len(output) == 2:
+        corners, M = output
+        t_corners = cv.perspectiveTransform(corners, M)
 
-mask = cv.inRange(hsv, low_green, high_green)
-kernel = np.ones((5, 5), np.uint8)
-mask = cv.erode(mask, kernel, iterations=1)
+        worldSpace = cv.polylines(worldSpace,[np.int32(t_corners)],True,(0,255,0),10, cv.LINE_AA)
+        img = cv.polylines(img,[np.int32(corners)],True,(0,255,255),10, cv.LINE_AA)
 
-
-contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-for cnt in contours:
-    area = cv.contourArea(cnt)
-    approx = cv.convexHull(cnt)#cv.approxPolyDP(cv.convexHull(cnt), 0.05*cv.arcLength(cnt, True), True)
-
-    if area > 10000:
-        cv.drawContours(outputRect, [approx], 0, 255, 1)
-        
-#smallkernel = np.ones((3,3))
-#outputRect = cv.erode(outputRect, smallkernel, iterations=3)
-#edges = cv.Canny(outputRect,50,150,apertureSize = 3)
-
-lines = cv.HoughLinesP(outputRect,1,np.pi/180,100,minLineLength=100,maxLineGap=10)
-filteredLines = filterLines(lines)
-
-print("Filtered {} lines down to {}".format(len(lines), len(filteredLines)))
-assert(len(filteredLines) >= 4), "That ain't enough sides!!!"
-    
-for line in filteredLines:
-    x1,y1,x2,y2 = line
-    cv.line(img4,(x1,y1),(x2,y2),(0,255,0),2)
-    
-corners = getCorners(filteredLines)
-drawCorners(img4, corners)
-
-#table_template_points = np.transpose(TEMPLATE_SCALE*np.array([[0,0,1],[515,0,1],[0,295,1],[515,295,1]]))
-#homogeneous_corners = np.transpose(np.hstack((corners, np.ones((corners.shape[0],1)))))
-
-table_template_points = np.transpose(TEMPLATE_SCALE*np.array([[0,0],[515,0],[0,295],[515,295]]))
-drawCorners(img4, np.transpose(table_template_points.astype(int)), (255,0,0))
-affine_corners = np.transpose(corners)
-
-affine_transform = np.linalg.lstsq(affine_corners, table_template_points, rcond=None)[0]
-transformedPoints = np.transpose(affine_corners @ affine_transform).astype(int)
-drawCorners(img4, transformedPoints, (0,0,255), 10)
-
-plt.imshow(cv.cvtColor(img4,cv.COLOR_BGR2RGB))
-plt.title('Image'), plt.xticks([]), plt.yticks([])
-plt.show()
-
-'''
-img2 = cv.imread('table.png', 0)
-img1 = cv.imread("template.jpg", 0)
-MIN_MATCH_COUNT = 100
-
-
-# Initiate SIFT detector
-sift = cv.SIFT_create()
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
-
-
-FLANN_INDEX_KDTREE = 1
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-
-search_params = dict(checks = 50)
-flann = cv.FlannBasedMatcher(index_params, search_params)
-matches = flann.knnMatch(des1,des2,k=2)
-# store all the good matches as per Lowe's ratio test.
-good = []
-for m,n in matches:
-    if True:#m.distance < 0.7*n.distance:
-        good.append(m)
-
-if len(good)>MIN_MATCH_COUNT:
-    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
-    M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
-    matchesMask = mask.ravel().tolist()
-    h,w = img1.shape
-    pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-    dst = cv.perspectiveTransform(pts,M)
-    print(dst)
-    img2 = cv.polylines(img2,[np.int32(dst)],True,(0,0,0),3, cv.LINE_AA)
-else:
-    print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
-    matchesMask = None
-    
-draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                   singlePointColor = None,
-                   matchesMask = matchesMask, # draw only inliers
-                   flags = 2)
-img3 = cv.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
-
-#img3=cv.drawKeypoints(img1,kp1,img1,flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-plt.imshow(img3),plt.show()
-'''
+        plt.subplot(121),plt.imshow(cv.cvtColor(img,cv.COLOR_BGR2RGB))
+        plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+        plt.subplot(122),plt.imshow(worldSpace)
+        plt.title('Transformed Image'), plt.xticks([]), plt.yticks([])
+        plt.show()
+    else:
+        plt.imshow(cv.cvtColor(output,cv.COLOR_BGR2RGB))
+        plt.title('Bugged Output'), plt.xticks([]), plt.yticks([])
+        plt.show()
